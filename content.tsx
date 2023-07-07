35c4518b14e2be7ba936d24cdd2b0de2e2ad6dec
nobleator <bn2471f9d@gmail.com>
2023-07-06 20:49:24 -0400

    Add inital eval functions using Overpass API

--- content.tsx
+++ content.tsx
@@ -25,29 +25,83 @@ const getGeocodedData = async (address) => {
     return response.data;
 }
 
-const getGeocodedDataCallback = (address, setGeocodedData) => {
+const evaluateNodes = (nodes, targetLat, targetLon) => {
+    let query = "";
+    nodes.forEach((node) => {
+        if (node.type === "ConditionalNode") {
+            query += "(";
+            query += evaluateNodes(node.children, targetLat, targetLon);
+            query += ")";
+        } else if (node.type === "ValueNode") {
+            // TODO: elements have 2 tags, not just 1, need to map "name" to user input rather than hardcoding
+            query += `nwr["name"="${node.tag}"](around:${node.value}, ${targetLat}, ${targetLon});`
+        }
+    });
+    return query;
+}
+
+const buildOverpassQuery = (anchorData, treeData) => {
+    let query = "[out:json];";
+    query += evaluateNodes(treeData.data, anchorData.data[0].lat, anchorData.data[0].lon);
+    query += ";out count;";
+    return query;
+}
+
+const evaluate = async (anchorData, treeData) => {
+    const query = buildOverpassQuery(anchorData, treeData);
+    const url = 'https://www.overpass-api.de/api/interpreter';
+    // TODO debounce or throttle these requests
+    const response = await fetch(url, {
+        "body": `data=${encodeURIComponent(query)}`,
+        "method": "POST",
+    });
+    const reader = response.body.pipeThrough(new TextDecoderStream()).getReader();
+    const {value, done} = await reader.read();
+    const jsonValue = JSON.parse(value);
+    const count = jsonValue.elements[0].tags["total"];
+    return count > 0;
+}
+
+const getGeocodedDataCallback = (anchorData, setAnchorData) => {
     const limitedGeocodedData = limiter.wrap(getGeocodedData);
-    limitedGeocodedData(address)
+    limitedGeocodedData(anchorData.address)
         .then((data) => {
-            return setGeocodedData(data);
+            const state = { ...anchorData };
+            // TODO: only store required fields (lat, lon)
+            state.data =  { lat: data[0].lat, lon: data[0].lon };
+            return setAnchorData(state);
         });
 }
 
-const evaluateTree = (anchorData) => {
-    const [treeData, _] = useStorage(STORAGE_KEY);
-    if (anchorData) {
-        const lat = anchorData[0].lat;
-        const lon = anchorData[0].lon;
-        return `${lat} | ${lon} | ${treeData[0].operator}`;
+const evaluateCallback = (treeData, anchorData, setAnchorData) => {
+    const limitedEval = limiter.wrap(evaluate);
+    if (anchorData && anchorData.data && anchorData.data.length > 0) {
+        limitedEval(anchorData, treeData)
+            .then((evalResult) => {
+                const state = { ...anchorData };
+                state.lastEvalTime = Date.now();
+                state.evalResult = evalResult;
+                return setAnchorData(state);
+            });
     }
-    return "...";
 }
 
 const ZillowOverlay = ({anchor}) => {
+    const [treeData, _] = useStorage(STORAGE_KEY);
     const anchorKey = anchor.element.innerText;
-    const [anchorData, setAnchorData] = useStorage(anchorKey);
-    if (!anchorData) {
-        getGeocodedDataCallback(anchorKey, setAnchorData);
+    const [anchorData, setAnchorData] = useStorage(anchorKey, {
+        key: anchorKey,
+        address: anchor.element.innerText,
+        data: null,
+        evalResult: null,
+        lastEvalTime: null,
+    });
+    // TODO: anchorData is not being persisted properly between page loads
+    if (anchorKey && !anchorData.data) {
+        getGeocodedDataCallback(anchorData, setAnchorData);
+    } 
+    if (anchorData.data && (anchorData.evalResult == null || anchorData.lastEvalTime < treeData.lastModified)) {
+        evaluateCallback(treeData, anchorData, setAnchorData);
     }
 
     return (
@@ -56,9 +110,9 @@ const ZillowOverlay = ({anchor}) => {
         style={{
             borderRadius: 4,
             padding: 4,
-            background: "pink"
+            background: (anchorData && anchorData.evalResult) ? "palegreen" : "pink"
         }}>
-        { evaluateTree(anchorData) }
+        { (!anchorData || anchorData.evalResult == null) ? "?" : anchorData.evalResult ? "✓" : "x" }
         </span>
     )
 }
