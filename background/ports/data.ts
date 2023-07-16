6533b65ea813e4586ac1a9e48b1cd4c3e1653b67
nobleator <bn2471f9d@gmail.com>
2023-07-16 09:08:25 -0400

    Simplify geocoded data
    Simplify the geocoded data results to only latitude & longitude. Also added some additional checks to short-circuit if data isn't present.

--- background/ports/data.ts
+++ background/ports/data.ts
@@ -42,12 +42,13 @@ const evaluateNodes = (nodes, targetLat, targetLon) => {
 
 const buildOverpassQuery = (anchorData, treeData) => {
     let query = "[out:json];";
-    query += evaluateNodes(treeData.data, anchorData.data[0].lat, anchorData.data[0].lon);
+    query += evaluateNodes(treeData.data, anchorData.lat, anchorData.lon);
     query += ";out count;";
     return query;
 }
 
 const evaluate = async (anchorData, treeData) => {
+    console.log(`evaluating ${anchorData.address}`);
     const query = buildOverpassQuery(anchorData, treeData);
     const url = 'https://www.overpass-api.de/api/interpreter';
     // TODO debounce or throttle these requests
@@ -56,14 +57,19 @@ const evaluate = async (anchorData, treeData) => {
         "method": "POST",
     });
     const count = await response.json().then(json => {
-        return json.elements[0].tags["total"];
+        if (json && json.elements && json.elements.length > 0) {
+            return json.elements[0].tags["total"];
+        } else {
+            return 0;
+        }
     });
     return count > 0;
 }
 
 const needsGeocodingAndEvaluation = (anchorData) => {
+    console.log(`checking for goecoding need: ${JSON.stringify(anchorData)}`);
     return !anchorData ||
-        (anchorData && !anchorData.geocodeData);
+        (anchorData && !(anchorData.lat || anchorData.lon));
 }
 
 const needsEvaluation = (anchorData, treeData) => {
@@ -76,28 +82,32 @@ const needsEvaluation = (anchorData, treeData) => {
 const handler: PlasmoMessaging.PortHandler = async (req, res) => {
     // Note: Can't use hooks within background worker as it is not a React component
     const storage = new Storage();
-    const treeData: any = await storage.get(STORAGE_KEY);
     const anchorKey = req.body.key;
     const address = req.body.address;
     let anchorData: any = await storage.get(anchorKey);
+    const treeData: any = await storage.get(STORAGE_KEY);
     const limitedGeocodedData = limiter.wrap(getGeocodedData);
     const limitedEval = limiter.wrap(evaluate);
     if (needsGeocodingAndEvaluation(anchorData)) {
         anchorData = {
             key: anchorKey,
             address: address,
-            geocodedData: null,
+            lat: null,
+            lon: null,
             evalResult: null,
             lastEvalTime: null,
         };
         const geocodedData = await limitedGeocodedData(anchorData.address)
             .then(x => { return x; });
-        anchorData.geocodedData = geocodedData;
-        const evalResult = await limitedEval(anchorData, treeData)
-            .then(x => { return x; });
-        anchorData.evalResult = evalResult;
-        anchorData.lastEvalTime = Date.now();
-        await storage.set(anchorKey, anchorData);
+        if (geocodedData && geocodedData.length > 0) {
+            anchorData.lat = geocodedData[0].lat;
+            anchorData.lon = geocodedData[0].lon;
+            const evalResult = await limitedEval(anchorData, treeData)
+                .then(x => { return x; });
+            anchorData.evalResult = evalResult;
+            anchorData.lastEvalTime = Date.now();
+            await storage.set(anchorKey, anchorData);
+        }
     } else if (needsEvaluation(anchorData, treeData)) {
         const evalResult = await limitedEval(anchorData, treeData)
             .then(x => { return x; });
