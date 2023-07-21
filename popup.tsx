cb8534ef10eea565b1207fa7099fbfcf9c47e425
nobleator <bn2471f9d@gmail.com>
2023-07-20 20:53:36 -0400

    Move config from options to popup
    Move preference tree configuration from options to popup

--- popup.tsx
+++ popup.tsx
@@ -1,26 +1,163 @@
-import { useState } from "react"
+import React from 'react';
+import { useStorage } from "@plasmohq/storage/hook"
+import Tree from './tree';
+
+export const STORAGE_KEY = "mvptl-tree";
+
+const handlePurge = () => {
+  if (confirm("Are you sure you want to purge your browser storage?")) {
+    chrome.storage.sync.clear();
+    chrome.storage.local.clear();  
+  }
+}
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [treeData, setTreeData] = useStorage(STORAGE_KEY, {
+    lastModified: Date.now(),
+    data: [
+      {
+        id: 1,
+        type: 'ConditionalNode',
+        operator: 'AND',
+        children: []
+      }
+    ]
+  });
+
+  const handlePrintTree = () => {
+    console.log(treeData);
+  };
+
+  const handleNodeFieldChange = (nodeId, field, newValue) => {
+    setTreeData((prevTreeData) => {
+      return {
+        ...prevTreeData,
+        lastModified: Date.now(),
+        data: updateNodeField(prevTreeData.data, nodeId, field, newValue)
+      };
+    });
+  };
+
+  const handleRemoveNode = (nodeId) => {
+    setTreeData((prevTreeData) => {
+      return {
+        ...prevTreeData,
+        lastModified: Date.now(),
+        data: removeNode(prevTreeData.data, nodeId)
+      };
+    });
+  };
+
+  const handleAddValueNode = (parentId) => {
+    setTreeData((prevTreeData) => {
+      return {
+        ...prevTreeData,
+        lastModified: Date.now(),
+        data: addNode(prevTreeData.data, parentId, 'ValueNode')
+      };
+    });
+  };
+
+  const handleAddConditionalNode = (parentId) => {
+    setTreeData((prevTreeData) => {
+      return  {
+        ...prevTreeData,
+        lastModified: Date.now(),
+        data: addConditionalNode(prevTreeData.data, parentId)
+      };
+    });
+  };
+
+  const updateNodeField = (nodes, nodeId, field, newValue) => {
+    return nodes.map((node) => {
+      if (node.id === nodeId) {
+        return { ...node, [field]: newValue };
+      } else if (node.children && node.children.length > 0) {
+        return { ...node, children: updateNodeField(node.children, nodeId, field, newValue) };
+      }
+      return node;
+    });
+  };
+
+  const removeNode = (nodes, nodeId) => {
+    return nodes.filter((node) => {
+      if (node.id === nodeId) {
+        return false;
+      } else if (node.children && node.children.length > 0) {
+        node.children = removeNode(node.children, nodeId);
+        return true;
+      }
+      return true;
+    });
+  };
+
+  const addNode = (nodes, parentId, nodeType) => {
+    return nodes.map((node) => {
+      if (node.id === parentId) {
+        const newNode = {
+          id: Date.now(),
+          type: nodeType,
+          tag: 'newTag',
+          operator: 'equals',
+          value: 'newValue',
+        };
+        if (node.children) {
+          node.children.push(newNode);
+        } else {
+          node.children = [newNode];
+        }
+      } else if (node.children && node.children.length > 0) {
+        node.children = addNode(node.children, parentId, nodeType);
+      }
+      return node;
+    });
+  };
+
+  const addConditionalNode = (nodes, parentId) => {
+    return nodes.map((node) => {
+      if (node.id === parentId) {
+        const newConditionalNode = {
+          id: Date.now(),
+          type: 'ConditionalNode',
+          operator: 'AND',
+          children: [
+            {
+              id: Date.now() + 1,
+              type: 'ValueNode',
+              tag: 'newTag',
+              operator: 'equals',
+              value: 'newValue',
+            },
+          ],
+        };
+        if (node.children) {
+          node.children.push(newConditionalNode);
+        } else {
+          node.children = [newConditionalNode];
+        }
+      } else if (node.children && node.children.length > 0) {
+        node.children = addConditionalNode(node.children, parentId);
+      }
+      return node;
+    });
+  };
 
   return (
     <div
       style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
+        width: 600,
       }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo3
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+      <h1>Settings</h1>
+      <span>Last modified: {treeData.lastModified}</span>
+      <Tree
+        data={treeData.data}
+        onNodeFieldChange={handleNodeFieldChange}
+        onRemoveNode={handleRemoveNode}
+        onAddValueNode={handleAddValueNode}
+        onAddConditionalNode={handleAddConditionalNode}
+      />
+      <button onClick={handlePrintTree}>Print Tree</button>
+      <button onClick={handlePurge}>Purge</button>
     </div>
   )
 }
